61067b8e19817ad7e22bd72d67105ab0d0cb69f8
CH MD <39788804+medchek@users.noreply.github.com>
2022-03-21 20:48:51 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <main>Sudoku</main>
+    </div>
+  );
+};
+
+export default App;
