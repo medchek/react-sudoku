ba3c3441b0be688642d8065b02951a4720b750b4
CH MD <39788804+medchek@users.noreply.github.com>
2022-03-22 20:15:15 +0100

    feat: add icon component

---
+++ src/components/Icon.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface Props {
+  icon: string;
+  className: string;
+}
+
+const Icon = ({ icon, className }: Props) => {
+  return (
+    <svg viewBox="0 0 24 24" preserveAspectRatio="none" className={className}>
+      <path fill="currentColor" className="fill-current" d={icon} />
+    </svg>
+  );
+};
+
+export default Icon;
