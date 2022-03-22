ea3ece213fc58b30d90ff04b7e323b30434e0412
CH MD <39788804+medchek@users.noreply.github.com>
2022-03-22 20:13:26 +0100

    feat: add Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#00FF97",
+        primaryLight: "#BCFFE8",
+        darkGrey: "#171717",
+        lightGrey: "#E3E3E3",
+        buttonGrey: "#F9F9F9",
+      },
+    },
+  },
+  plugins: [],
+};
