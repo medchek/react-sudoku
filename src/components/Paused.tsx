45ea6dc8c3ace8c897f4535cf6e345bc459fca47
CH MD <39788804+medchek@users.noreply.github.com>
2022-04-05 21:54:02 +0100

    feat: add component for when the game is paused

--- src/components/Paused.tsx
+++ src/components/Paused.tsx
@@ -15,7 +15,7 @@ const Paused = () => {
       className="flex flex-col items-center justify-center absolute top-0 bottom-0 left-0 right-0 w-full h-full border-4 border-primaryLight z-100 bg-white space-y-2"
     >
       <p className="font-medium text-2xl text-darkGrey w-1/2 text-center">
-        The game is currenty <span className="font-bold">paused</span>
+        The game is currenty <span className="font-bold">Paused</span>
       </p>
       <button
         type="button"
