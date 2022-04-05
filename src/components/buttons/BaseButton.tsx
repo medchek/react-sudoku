ab784fe73bcd707e40dc7c34376004d27281e8d5
CH MD <39788804+medchek@users.noreply.github.com>
2022-04-05 15:23:22 +0100

    feat: add disabled status for base button

--- src/components/buttons/BaseButton.tsx
+++ src/components/buttons/BaseButton.tsx
@@ -5,16 +5,24 @@ interface Props {
   className: string;
   onClick?: () => void;
   customColors?: boolean;
+  disable?: boolean;
 }
 
-const BaseButton = ({ children, className, onClick, customColors }: Props) => {
+const BaseButton = ({
+  children,
+  className,
+  onClick,
+  disable,
+  customColors,
+}: Props) => {
   return (
     <button
-      className={`relative flex justify-center items-center w-14 h-14 rounded-md shadow-md active:scale-[1.03] ${className} ${
-        !customColors ? "bg-buttonGrey" : ""
+      className={`relative flex justify-center border border-slate-200/50 items-center w-14 h-14 rounded-md shadow-md active:scale-[1.03] disabled:cursor-not-allowed  disabled:ring-0 disabled:text-zinc-300 ${className} ${
+        !customColors ? "bg-white" : ""
       }`}
       onClick={onClick}
       type="button"
+      disabled={disable ?? false}
     >
       {children}
     </button>
