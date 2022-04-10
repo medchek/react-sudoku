7beea85eab06f025aa267e6a76cb9e4d316227a7
CH MD <39788804+medchek@users.noreply.github.com>
2022-04-10 22:14:36 +0100

    feat: stop timer when tab is not focused

--- src/views/Sudoko.tsx
+++ src/views/Sudoko.tsx
@@ -1,8 +1,32 @@
 import Grid from "../components/Grid";
 import Logo from "../components/svgs/Logo";
 import Operations from "../components/Operations";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { pauseTimer, unpauseTimer } from "../store/slices/timerSlice";
+import { useAppSelector } from "../store/storeHooks";
+import { RootState } from "../store/store";
 
 const Sudoko = () => {
+  const dispatch = useDispatch();
+  const diffuculty = useAppSelector(
+    (state: RootState) => state.grid.difficulty
+  );
+  useEffect(() => {
+    const onVisibilityChange = () => {
+      if (diffuculty === null) return;
+      if (document.visibilityState === "hidden") {
+        dispatch(pauseTimer());
+      } else {
+        dispatch(unpauseTimer());
+      }
+    };
+    document.addEventListener("visibilitychange", onVisibilityChange);
+    return () => {
+      document.removeEventListener("visibilitychange", onVisibilityChange);
+    };
+  }, [dispatch, diffuculty]);
+
   return (
     <div className="flex flex-col px-4 sm:px-8 md:px-10 lg:px-20 xl:px-40 2xl:px-72 w-full h-full overflow-hidden">
       <header className="w-full h-12 min-h-[3rem] sm:h-14 sm:min-h-[3.5rem] pt-1 md:pt-2">
