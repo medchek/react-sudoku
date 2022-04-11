b3092c05ec401d2569fdfb5c770ea8a23597f324
CH MD <39788804+medchek@users.noreply.github.com>
2022-04-11 23:26:32 +0100

    feat: implement app routes

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { store } from "../store/store";
 import { Provider } from "react-redux";
-import Sudoko from "./Sudoko";
+import { Route, Routes } from "react-router";
+// import Sudoko from "./Sudoko";
+
+const Sudoko = lazy(() => import("./Sudoko"));
+const Home = lazy(() => import("./Home"));
 
 const App = () => {
   return (
     <Provider store={store}>
       <div className="h-screen max-h-screen min-h-screen w-screen overflow-hidden">
-        <Sudoko />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Suspense fallback={"Loading..."}>
+                <Home />
+              </Suspense>
+            }
+          />
+          <Route
+            path="play"
+            element={
+              <Suspense fallback={"Loading..."}>
+                <Sudoko />
+              </Suspense>
+            }
+          />
+        </Routes>
       </div>
     </Provider>
   );
