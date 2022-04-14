c60b27984ca9f4f8378fe621cc228b050b13a679
CH MD <39788804+medchek@users.noreply.github.com>
2022-04-14 17:24:35 +0100

    feat: update dark theme mode to class based

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
@@ -9,6 +10,9 @@ module.exports = {
         lightGrey: "#E3E3E3",
         buttonGrey: "#F9F9F9",
         darkBg: "#070707",
+        darkGreyBg: "#171717",
+        darkCellGrey: "#292828",
+        darkCellGreen: "#42A885",
       },
       scale: {
         103: "1.03",
