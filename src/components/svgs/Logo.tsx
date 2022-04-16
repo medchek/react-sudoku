2133d3ede9aad814dfc1a778fa10cb0128a750f9
CH MD <39788804+medchek@users.noreply.github.com>
2022-04-16 16:24:30 +0100

    refactor: rename logo component name

--- src/components/svgs/Logo.tsx
+++ src/components/svgs/Logo.tsx
@@ -4,7 +4,7 @@ interface Props {
   className: string;
 }
 
-const SvgComponent = (props: Props) => (
+const Logo = (props: Props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 44 44"
@@ -24,4 +24,4 @@ const SvgComponent = (props: Props) => (
   </svg>
 );
 
-export default SvgComponent;
+export default Logo;
