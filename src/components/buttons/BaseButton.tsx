d9993c28453e23bbe6ea87b59087a0578e4b7352
CH MD <39788804+medchek@users.noreply.github.com>
2022-04-20 16:13:56 +0100

    feat: add ability to set custom size for base button

--- src/components/buttons/BaseButton.tsx
+++ src/components/buttons/BaseButton.tsx
@@ -5,6 +5,7 @@ interface Props {
   className: string;
   onClick?: () => void;
   customColors?: boolean;
+  customSize?: boolean;
   disable?: boolean;
   noBorder?: boolean;
 }
@@ -15,11 +16,12 @@ const BaseButton = ({
   onClick,
   disable,
   customColors,
+  customSize,
   noBorder,
 }: Props) => {
   return (
     <button
-      className={`relative flex justify-center items-center w-12 h-12 sm:w-14 sm:h-14 rounded-md shadow-md active:scale-[1.03] disabled:cursor-not-allowed  disabled:ring-0  outline-none ${className} ${
+      className={`relative flex justify-center items-center rounded-md shadow-md active:scale-[1.03] disabled:cursor-not-allowed disabled:ring-0  outline-none ${className} ${
         !customColors
           ? "bg-white dark:bg-zinc-900 text-darkGrey dark:text-zinc-50  disabled:text-zinc-300 dark:disabled:opacity-30 dark:disabled:text-zinc-700"
           : ""
@@ -27,8 +29,9 @@ const BaseButton = ({
         noBorder === undefined || noBorder === false
           ? "border border-slate-200/50 dark:border-zinc-700"
           : ""
-      }
-      `}
+      } ${customSize !== true ? "w-11 h-11 sm:w-14 sm:h-14" : ""}${
+        disable ? " pointer-events-none" : ""
+      }`}
       onClick={onClick}
       type="button"
       disabled={disable ?? false}
