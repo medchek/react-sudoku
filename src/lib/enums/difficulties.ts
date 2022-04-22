0d84cea9edb13f17dc7a867ec39142a9b809f7d5
CH MD <39788804+medchek@users.noreply.github.com>
2022-04-22 18:47:41 +0100

    feat: implement enum for difficulty hints count

--- src/lib/enums/difficulties.ts
+++ src/lib/enums/difficulties.ts
@@ -4,3 +4,10 @@ export enum Difficulty {
   Hard,
   Insane,
 }
+
+export enum DifficultyHintCount {
+  Easy = 36, // 36 - 46
+  Medium = 32, // 32 - 35
+  Hard = 28, // 28 - 31
+  Insane = 17, // 17 - 27
+}
